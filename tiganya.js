5d629b34aa9c3c08dcd519bfcb8070f84d623d72
ismayadanuwati <103593869+ismayadanuwati@users.noreply.github.com>
2022-04-13 23:40:43 +0700

    Create tiganya.js

---
+++ tiganya.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '7b7c858d1be0b864fe9d0c01b20ae60a',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/7b7c858d1be0b864fe9d0c01b20ae60a/invoke.js"></scr' + 'ipt>');
